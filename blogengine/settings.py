6399b64b80cfa752b8241b3debcc2eaa003c9ca8
Vitalii Kostyreva <69001084+Vitalikys@users.noreply.github.com>
2022-06-13 02:38:24 +0300

    upd Settings.py
    debug = True

--- blogengine/settings.py
+++ blogengine/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yog8+k-7gd4+j3*1q=*bmwc#y!3^e%9^n4t2qr(51bvo33+cab'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 LOGGING = {
     'version': 1,
