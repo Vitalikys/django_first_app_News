03eb1a1ff056fa0114cbfc5da929cefd64dfb8ff
Vitalii Kostyreva <vitalikys87@i.ua>
2022-06-13 13:59:44 +0300

    push to site settings

--- blogengine/settings.py
+++ blogengine/settings.py
@@ -15,7 +15,7 @@ import dj_database_url
 import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-yog8+k-7gd4+j3*1q=*bmwc#y!3^e%9^n4t2qr(51bvo33+cab
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['<url>']
-
+ALLOWED_HOSTS = ['<url>','.herokuapp.com']
+CSRF_TRUSTED_ORIGINS = ["https://newskost.herokuapp.com"]
 # Application definition
 
 INSTALLED_APPS = [
